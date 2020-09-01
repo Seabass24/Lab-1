e9e1698eb718196001eec6ad3ea1e177e69734fd
Sebastian Rodriguez <spr5501@psu.edu>
2020-09-01 21:47:03 +0000

    \n

--- main.c
+++ main.c
@@ -9,6 +9,6 @@ int main(void) {
   char *temp=readline("Enter temperature in celsius: ");
   temperature = atof(temp);
   f= temperature* 9/5 + 32;
-  printf("%f° in Celsius is equivalent to %f° Fahrenheit.",temperature,f);
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n",temperature,f);
   return 0;
 }
